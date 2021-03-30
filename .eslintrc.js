2462da85eb3f5354b5001b77a83889912b85287e
Brian Bugh <brainbag@gmail.com>
2021-03-30 13:10:17 -0500

    Add separate tsconfig.eslint.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.eslint.json',
     sourceType: "module"
   },
   env: {
