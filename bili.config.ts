504f232b243c3c7ebcc1fadd1466786cb43ff264
Brian Bugh <brainbag@gmail.com>
2021-03-30 16:56:46 -0500

    Add bili for building

---
+++ bili.config.ts
@@ -0,0 +1,28 @@
+import { Config } from "bili";
+
+const config: Config = {
+  input: "break_eternity.ts",
+  extendRollupConfig: (config) => {
+    config.outputConfig.exports = "auto";
+    return config;
+  },
+  output: {
+    format: ["umd", "umd-min", "esm", "cjs"],
+    moduleName: "Decimal",
+    sourceMap: false,
+    fileName: (context, defaultFileName) => {
+      switch (context.format) {
+        case "umd":
+          return context.minify ? "break_infinity.min.js" : "break_infinity.js";
+        case "esm":
+          return "break_infinity.esm.js";
+        case "cjs":
+          return "break_infinity.cjs.js";
+        default:
+          return defaultFileName;
+      }
+    },
+  },
+};
+
+export default config;
