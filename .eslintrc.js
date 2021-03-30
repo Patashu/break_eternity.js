e566d963f37cfed8390480eecdbd28467698be1d
Brian Bugh <brainbag@gmail.com>
2021-03-30 10:07:44 -0500

    Add linters for TS

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: "module"
+  },
+  env: {
+    commonjs: true,
+    mocha: true
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "prettier/prettier": ["error"],
+  }
+};
