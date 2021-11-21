4f3756142000bb40b8cfc707356f2b78d5e5cff0
Timothy Stiles <Patashu@hotmail.com>
2021-11-21 15:00:24 +1100

    Update src/index.ts
    Co-authored-by: Brian Bugh <438465+bbugh@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -160,8 +160,7 @@ function d_lambertw(z: Decimal, tol = 1e-10): Decimal {
   if (z === Decimal.dZero) {
     return z;
   }
-  // FIXME: Seems like this should be == Decimal.dOne?
-  if (z === 1) {
+  if (z === Decimal.dOne) {
     //Split out this case because the asymptotic series blows up
     return OMEGA; // FIXME: Seems like this should return D(OMEGA)?
   }
