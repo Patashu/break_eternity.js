5d89b4f40aa33f6ede2ac385d8aa94ca6b570419
mcpower <mcpower@users.noreply.github.com>
2022-03-17 22:13:01 +1100

    Fix setting .m to 0 not setting layer and exponent
    To prevent this from happening in the future, this commit also enables
    eslint's no-unused-expression lint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,7 @@ module.exports = {
     ],
     "@typescript-eslint/restrict-plus-operands": 0, // A lot of strings are built with +
     "@typescript-eslint/no-this-alias": 0, // `this` is aliased in several places
-  }
+    "no-unused-expressions": "off",
+    "@typescript-eslint/no-unused-expressions": ["error"],
+  },
 };
